90581b171ed0e890f06fdef33414fca856d3b14c
Shannen <shannen.ye@gmail.com>
2018-02-20 22:25:58 -0500

    finished creating store and rendering Provider

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,10 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'stage-2', 'env']
+        }
       }
     ]
   }
